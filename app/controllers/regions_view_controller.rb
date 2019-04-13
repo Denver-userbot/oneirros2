9a12193e3cf8b8a2a5e8eeffd615a37a6a497389
Fira <loyauflorian@gmail.com>
2019-04-13 18:17:38 +0200

    Add Datatables, implement RegionsViewController Preview

---
+++ app/controllers/regions_view_controller.rb
@@ -0,0 +1,26 @@
+require 'pp'
+
+class RegionsViewController < ApplicationController 
+  def index
+    @results_by_id = Hash.new
+
+    # Query ActiveRecord for static data
+    regions = RivalRegion.all
+    regions.each do |region|
+      region_id = region[:rivals_id]
+
+      @results_by_id[region_id] = Hash.new
+      @results_by_id[region_id][:id] = region_id
+      @results_by_id[region_id][:region] = region
+    end
+
+    # Query Influx in Bulk and merge
+    RivalRegionMetrics.all.group(:rivals_id).order(time: :desc).each do |metric|
+      region_id = metric["rivals_id"].to_i
+
+      @results_by_id[region_id][:metrics] = metric
+    end
+  
+    render 'regions_view/index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 
 Rails.application.routes.draw do
   root 'homepage#index'
+  get 'regions_view', to: 'regions_view#index'
   resources :regions
 end
