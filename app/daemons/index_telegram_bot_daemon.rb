692d2cb4e340b9d41591045af9402e0413dbf69e
Fira <loyauflorian@gmail.com>
2019-04-14 13:45:12 +0200

    Bugfixes for Index Telegram Bot

--- app/daemons/index_telegram_bot_daemon.rb
+++ app/daemons/index_telegram_bot_daemon.rb
@@ -8,29 +8,54 @@ class IndexTelegramBotDaemon < TelegramBotDaemon
   def run 
     Telegram::Bot::Client.run(@bot_token) do |bot|
       bot.listen do |message|
+        puts "@#{message.chat.username} << #{message.text}"
+
         if message.text == '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello there ! Tell me what indice you want (med|edu|mil|dev) and the amount and i'll come back to you with the required building amount as per our last data. For example, send me 'mil 3' to get stats on Military 3.")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello there ! Tell me what indice you want (med|edu|mil|dev) and the amount and i'll come back to you with the required building amount as per our last data. For example, send me 'mil 3' to get stats on Military 3.\n\nNote the data can be delayed by up to ten minutes. This bot is part of the Oneirros Project (http://oneirros.xyz:3000) and still in development. For any inquiries, please contact @EphyRaZy")
         else
-          index = message.text.split(' ').first
+          index = message.text.split(' ').first.downcase
           level = message.text.split(' ').last.to_i
           # Detect relevant building
           case index
           when 'med'
-            building = "hospital"  
+            building = "hospital"
+            display_building = "Hospitals"
+            display_index = "Medical"
           when 'mil'
             building = "military_base"
+            display_building = "Military Bases"
+            display_index = "Military"
           when 'edu'
             building = "school"
+            display_building = "Schools"
+            display_index = "Education"
           when 'dev'
             building = "housing_fund"
+            display_building = "Housing Fund"
+            display_index = "Development"
           end
 
           if building.nil? 
-             bot.api.send_message(chat_id: message.chat.id, text: "Sorry, I don't recognize that indice.")
+             bot.api.send_message(chat_id: message.chat.id, text: "Sorry, I don't recognize that indice. Type /start for usage information.")
+          elsif level < 1 or level > 10
+             bot.api.send_message(chat_id: message.chat.id, text: "Please enter an indice level from 1 to 10. Type /start for usage information.")
 	  else
+             found = false
              # Query influx for data
-             RivalRegionMetrics.all.select("#{index}_index", :rivals_id, "min(#{building})").where("#{index}_index = #{level}").order(time: :desc).limit(1).each do |metric|
-               bot.api.send_message(chat_id: message.chat.id, text: "According to my last data (#{metric["time"]}):\nMinimum Amount for #{index} #{level} is #{metric["min"]} #{building}")
+             RivalRegionMetrics.all.select("#{index}_index", :rivals_id, "min(#{building})").where("#{index}_index = #{level}").time('5m').fill(:none).order(time: :desc).limit(3).each do |metric|
+               if not found 
+                 found = true
+
+                 if metric["min"].nil?
+                   bot.api.send_message(chat_id: message.chat.id, text: "Sorry, something went wrong with the database query.\nThis is a known bug and we're looking into fixing it.\nTry again in a moment !")
+                 else
+                   bot.api.send_message(chat_id: message.chat.id, text: "According to my last data:\nMinimum Amount for #{display_index} #{level} is #{metric["min"]} #{display_building}")
+                 end
+               end
+             end
+
+             if not found
+               bot.api.send_message(chat_id: message.chat.id, text: "Sorry, no recent data found. This likely means background data process crashed. Try again later !")
              end
           end
 
