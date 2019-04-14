e9e49955f2d223414caf6a8314759c977308e624
Fira <loyauflorian@gmail.com>
2019-04-14 10:10:38 +0200

    Switch Worker to use Singleton Sched for multiple workers integration

--- app/workers/authed_bulk_worker.rb
+++ app/workers/authed_bulk_worker.rb
@@ -3,7 +3,7 @@ require 'rufus-scheduler'
 class AuthedBulkWorker < OneirrosWorker
   def initialize(spider_class, every)
     @spider = spider_class.new
-    @sched = Rufus::Scheduler.new
+    @sched = Rufus::Scheduler.singleton
 
     @sched.every every do
       @spider.go
