8a468e8c8b9be3aa2c91560b2dd5655419083500
Fira <loyauflorian@gmail.com>
2019-04-15 13:33:20 +0200

    regions#show and regions#index now colour indices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  # Slow due to instanciating a new engine. Care !
+  def render_haml(template, locals = {})
+    engine = Haml::Engine.new(template.gsub(/^#{template[/^\s+/]}/, ''))
+    engine.render(self, locals)
+  end
+
+  def get_med_index_class(value)
+     return case
+       when value >= 8 then "uk-text-success"
+       when value >= 6 then "uk-text-warning"
+       when true then "uk-text-danger"
+     end
+  end
+  def get_mil_index_class(value)
+     return case
+       when value >= 6 then "uk-text-success"
+       when value >= 4 then "uk-text-warning"
+       when true then "uk-text-danger"
+     end
+  end
+  def get_edu_index_class(value)
+     return case
+       when value >= 8 then "uk-text-success"
+       when value >= 6 then "uk-text-warning"
+       when true then "uk-text-danger"
+     end
+  end
+  def get_dev_index_class(value)
+     return case
+       when value >= 6 then "uk-text-success"
+       when value >= 2 then "uk-text-warning"
+       when true then "uk-text-danger"
+     end
+  end
 end
