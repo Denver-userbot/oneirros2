1bfe57b465831247be218defbd10b662232b0ad2
Fira <loyauflorian@gmail.com>
2019-04-15 09:43:24 +0200

    Properly configure web-console for Dev

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Oneirros2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.web_console.whitelisted_ips = Rails.application.credentials.dig(:webconsole_whitelist)
+    config.web_console.whiny_requests = false
   end
 end
